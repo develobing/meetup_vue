21673e3b702e6c1aaea45c98a218ca90a9bfa852
Robin <philoin88@gmail.com>
2019-05-19 15:28:59 +0900

    router 및 첫 페이지 작업

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="sideNav" fixed>
+    <v-navigation-drawer temporary v-model="sideNav" fixed>
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>View Meetups</v-list-tile-content>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+
     <v-toolbar dark class="primary">
       <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer;">DevMeetup</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>View Meetups
+        <v-btn flat v-for="item in menuItems" :key="item.title" router :to="item.link">
+          <v-icon left>{{item.icon}}</v-icon>
+          {{item.title}}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    <main></main>
+
+    <main>
+      <router-view></router-view>
+    </main>
   </v-app>
 </template>
 
@@ -28,7 +35,14 @@
 export default {
   data() {
     return {
-      sideNav: false
+      sideNav: false,
+      menuItems: [
+        { icon: "supervisor_account", title: "View Meetups", link: "/meetups" },
+        { icon: "room", title: "Oranize Meetup", link: "/meetup/new" },
+        { icon: "person", title: "Profile", link: "/profile" },
+        { icon: "face", title: "Sign up", link: "/signin" },
+        { icon: "lock_open", title: "Sign in", link: "/signup" }
+      ]
     };
   }
 };
---
+++ src/components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetups</v-btn>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap class="mt-2">
+      <v-flex xs-12>
+        <v-carousel>
+          <v-carousel-item v-for="meetup in meetups" :src="meetup.imageUrl" :key="meetup.id">
+            <!-- <v-card>
+              <v-card-text class="text-sm-center">{{meetup.title}}</v-card-text>
+            </v-card>-->
+            <div class="title">{{meetup.title}}</div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    meetups: [
+      {
+        id: "sdafskdajhfskda23",
+        title: "Meetup in Newyork",
+        imageUrl:
+          "https://images.musement.com/cover/0001/93/thumb_92294_cover_header.jpeg"
+      },
+      {
+        id: "11sdafsk2sdafkda223",
+        title: "Meetup in Paris",
+        imageUrl:
+          "https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg"
+      }
+    ]
+  })
+};
+</script>
+
+<style scoped>
+.title {
+  /* position: absolute; */
+  /* bottom: 50px; */
+  color: white;
+  background-color: rgba(0, 0, 0, 0.5);
+  font-size: 2em;
+  padding: 20px;
+  margin: auto 0;
+  text-align: center;
+}
+</style>
